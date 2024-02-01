709f53b98fc0f8635cb18d8cf05b02cddbda455c
Lussati Santos <90448512+lussatisantos@users.noreply.github.com>
2024-02-01 22:19:33 +0000

    NumeroPrimo

---
+++ 01/WpfNumerosPrimosM1/FerramentasMagicas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfNumerosPrimosM1
+{
+    internal class FerramentasMagicas
+    {
+        static public Boolean VerificaNumeroPrimo(int n) { 
+            
+            Boolean retorno = false;
+
+            int count = 0;
+            if(n == 1)
+            {
+               retorno = true;
+            } else {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % 1 == 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == 2) { 
+                    retorno |= true;
+                }
+            }
+            return retorno;
+        }
+    }
+}
---
+++ 01/WpfNumerosPrimosM1/NumeroPrimo.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfNumerosPrimosM1
+{
+    /// <summary>
+    /// Interaction logic for NumeroPrimo.xaml
+    /// </summary>
+    public partial class NumeroPrimo : Window
+    {
+        public NumeroPrimo()
+        {
+            InitializeComponent();
+        }
+
+        private void tbValor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btExecutar_Click(object sender, RoutedEventArgs e)
+        {
+            int n = Convert.ToInt32(tbValor.Text);
+
+            if(FerramentasMagicas.VerificaNumeroPrimo(n) == true)
+            {
+                lbResult.Content = "O numero informado representa um numero primo";
+            } else
+            {
+                lbResult.Content = "O numero informado nao representa um numero primo";
+            }
+        }
+    }
+}
